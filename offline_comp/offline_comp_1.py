c26abb004367c7de49b5cb5d3544d1eb6a8ee84c
王乾 <1092521171@qq.com>
2016-09-12 22:16:18 +0800

    2016.09.12 night commit

--- offline_comp/offline_comp_1.py
+++ offline_comp/offline_comp_1.py
@@ -5,17 +5,22 @@ import platform
 # 获得计算机操作系统的类型
 os = platform.platform()
 print os
-# 用户表及商品表地址，分操作系统
-if os.startswith("Windows"):
+# 用户表及商品表地址，分操作系统 Windows and Mac
+if os.startswith("Windows"):  # windows系统
     base_path = 'C:\\Users\\dell\\Desktop\\file\\tianchi\\fresh_comp_offline'
-else:  # mac系统
-    # user_table_path = ''
+    user_table = open(base_path+'\\'+'tianchi_fresh_comp_train_user.csv')
+    item_table = open(base_path+'\\'+'tianchi_fresh_comp_train_item.csv')
+elif os.startswith('Darwin'):  # mac系统
+    base_path = '/Users/Skywice/Desktop/Python_App/tianchi_data/fresh_comp_offline'
+    user_table = open(base_path+'/'+'tianchi_fresh_comp_train_user.csv')
+    item_table = open(base_path+'/'+'tianchi_fresh_comp_train_item.csv')
+
+user_table_content = user_table.readlines()
+line_num = len(user_table_content)
+for i in range(10):
+    # print user_table_content[i]
     pass
 
-user_table = open(os.path.join(base_path, 'tianchi_fresh_comp_train_user.csv'))
-item_table = open(os.path.join(base_path, 'tianchi_fresh_comp_train_item.csv'))
-
-user_table_file = user_table.readlines()
-line_num = len(user_table_file)
-for i in range(line_num):
-    print user_table_file[i]
+# close files, can't be forgot
+user_table.close()
+item_table.close()
